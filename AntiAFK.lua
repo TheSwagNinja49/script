8b522bad3f6d079b0d2d0cd7299c49d980182597
TheSwagNinja49 <80137171+TheSwagNinja49@users.noreply.github.com>
2022-06-13 00:32:44 -0700

    Update AntiAFK.lua

--- AntiAFK.lua
+++ AntiAFK.lua
@@ -16,4 +16,4 @@ ab.Size=UDim2.new(0,370,0,44)ab.Font=Enum.Font.ArialBold;ab.Text="Status: Active
 ab.TextColor3=Color3.new(0,1,1)ab.TextSize=20;local bb=game:service'VirtualUser'
 game:service'Players'.LocalPlayer.Idled:connect(function()
 bb:CaptureController()bb:ClickButton2(Vector2.new())
-ab.Text="Roblox tried to kick u but i kicked him instead"wait(2)ab.Text="Status : Active"end)
+ab.Text="Roblox tried to kick you, but the script successfully prevented you from being kicked."wait(2)ab.Text="Status : Active"end)
